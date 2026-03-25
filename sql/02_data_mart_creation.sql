96ecbb1da61f9172f2cee23be861645c64c77e41
Abigail Larkai <larkaiabi@gmail.com>
2026-03-25 13:51:22 +0000

    Refactor SQL for fact_citibike_trips table creation

--- sql/02_data_mart_creation.sql
+++ sql/02_data_mart_creation.sql
@@ -3,44 +3,59 @@
 CREATE OR REPLACE TABLE `data-pipeline-project-485214.cyclist_data.fact_citibike_trips` AS
 
 SELECT
-  DATE_ADD(DATE(TRI.starttime), INTERVAL 5 YEAR) AS trip_date,
-  TRI.usertype AS user_type,
-
-  ZIPSTART.zip_code AS start_zip_code,
-  ZIPEND.zip_code AS end_zip_code,
-
-  ROUND(CAST(TRI.tripduration / 60 AS INT64), -1) AS trip_minutes_bucket,
-
-  COUNT(1) AS trip_count,
-
-  AVG(WEA.temp) AS avg_temperature,
-  AVG(WEA.wdsp) AS avg_wind_speed,
-  SUM(WEA.prcp) AS total_precipitation
-
-FROM `bigquery-public-data.new_york_citibike.citibike_trips` AS TRI
-
-JOIN `bigquery-public-data.geo_us_boundaries.zip_codes` ZIPSTART
-ON ST_WITHIN(
-  ST_GEOGPOINT(TRI.start_station_longitude, TRI.start_station_latitude),
-  ZIPSTART.zip_code_geom
-)
-
-JOIN `bigquery-public-data.geo_us_boundaries.zip_codes` ZIPEND
-ON ST_WITHIN(
-  ST_GEOGPOINT(TRI.end_station_longitude, TRI.end_station_latitude),
-  ZIPEND.zip_code_geom
-)
-
-JOIN `bigquery-public-data.noaa_gsod.gsod20*` WEA
-ON PARSE_DATE("%Y%m%d", CONCAT(WEA.year, WEA.mo, WEA.da)) = DATE(TRI.starttime)
-
+  TRI.usertype,
+  ZIPSTART.zip_code AS zip_code_start,
+  ZIPSTARTNAME.borough borough_start,
+  ZIPSTARTNAME.neighborhood AS neighborhood_start,
+  ZIPEND.zip_code AS zip_code_end,
+  ZIPENDNAME.borough borough_end,
+  ZIPENDNAME.neighborhood AS neighborhood_end,
+  -- Since this is a fictional dashboard, you can add 5 years to make it look recent
+  DATE_ADD(DATE(TRI.starttime), INTERVAL 5 YEAR) AS start_day,
+  DATE_ADD(DATE(TRI.stoptime), INTERVAL 5 YEAR) AS stop_day,
+  WEA.temp AS day_mean_temperature, -- Mean temp
+  WEA.wdsp AS day_mean_wind_speed, -- Mean wind speed
+  WEA.prcp day_total_precipitation, -- Total precipitation
+  -- Group trips into 10 minute intervals to reduces the number of rows
+  ROUND(CAST(TRI.tripduration / 60 AS INT64), -1) AS trip_minutes,
+  COUNT(TRI.bikeid) AS trip_count
+FROM
+  `bigquery-public-data.new_york_citibike.citibike_trips` AS TRI
+INNER JOIN
+  `bigquery-public-data.geo_us_boundaries.zip_codes` ZIPSTART
+  ON ST_WITHIN(
+    ST_GEOGPOINT(TRI.start_station_longitude, TRI.start_station_latitude),
+    ZIPSTART.zip_code_geom)
+INNER JOIN
+  `bigquery-public-data.geo_us_boundaries.zip_codes` ZIPEND
+  ON ST_WITHIN(
+    ST_GEOGPOINT(TRI.end_station_longitude, TRI.end_station_latitude),
+    ZIPEND.zip_code_geom)
+INNER JOIN
+  `bigquery-public-data.noaa_gsod.gsod20*` AS WEA
+  ON PARSE_DATE("%Y%m%d", CONCAT(WEA.year, WEA.mo, WEA.da)) = DATE(TRI.starttime)
+INNER JOIN
+  `data-pipeline-project-485214.cyclist_data.nyc_zipcodes` AS ZIPSTARTNAME
+  ON ZIPSTART.zip_code = CAST(ZIPSTARTNAME.zip AS STRING)
+INNER JOIN
+  `data-pipeline-project-485214.cyclist_data.nyc_zipcodes` AS ZIPENDNAME
+  ON ZIPEND.zip_code = CAST(ZIPENDNAME.zip AS STRING)
 WHERE
-  WEA.wban = '94728'
+  -- This takes the weather data from one weather station
+  WEA.wban = '94728' -- NEW YORK CENTRAL PARK
+  -- Use data from 2014 and 2015
   AND EXTRACT(YEAR FROM DATE(TRI.starttime)) BETWEEN 2014 AND 2015
-
 GROUP BY
-  trip_date,
-  user_type,
-  start_zip_code,
-  end_zip_code,
-  trip_minutes_bucket;
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13
